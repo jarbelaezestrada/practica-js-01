6a6619ac16886b775bab94e22e0ef2ae587ab8bf
jarbelaezestrada <julianaarbelaezestrada@gmail.com>
2023-08-30 22:15:37 -0500

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,28 @@
+const nombre = "Juliana Estrada";
+
+let edad = 29;
+
+let nota = 8;
+
+if (nombre === "Juliana Estrada" && nota > 7){
+    console.log("Hola " + nombre + " su nota es = " + nota);
+}else {
+    console.log("Hola nombre no existe");
+}
+
+
+const a = "Positivo";
+const b = "Negativo";
+
+let num1 = 23;
+let num2 = -10;
+
+if (a == "Positivo" && a <= 30 ) {
+    return "El numero es " + a;
+
+}else {
+    return "El numero es " + b;
+
+}
+
+
---
+++ function.js
@@ -0,0 +1,11 @@
+function areaCircunferencia(radio) {
+    const pi = 3.1415169;
+    const resultado = pi * radio * radio;
+    return resultado;
+}
+
+const result = areaCircunferencia(520);
+const result1 = areaCircunferencia(2427);
+const result2 = areaCircunferencia(830);
+const result3 = areaCircunferencia(125);
+
