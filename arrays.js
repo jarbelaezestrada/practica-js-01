f442a45729a469db286e2fdf97048376bce34213
jarbelaezestrada <julianaarbelaezestrada@gmail.com>
2023-09-02 19:03:45 -0500

    Arrays

--- arrays.js
+++ arrays.js
@@ -1,1 +1,31 @@
-let frutas = ["Manzana", "Fresa", "Pera", "Uva", "Piña", "Coco"];
+//Arrays
+
+let frutas = [];
+
+frutas[0] = "Manzana";
+frutas[1] = "Pera";
+frutas[2] = "Sandia";
+frutas[3] = "Uva";
+frutas[4] = "Piña";
+frutas[5] = "Melon";
+
+//frutas[1] = "platano";
+//console.log(frutas);
+
+let frutas1 = ["Manzana", "Fresa", "Pera", "Uva", "Piña", "Coco"];
+
+console.log(frutas1);
+
+//Ejercicio Array de familia.
+
+
+let miFamilia = ["Isabel", "Sebastian", "Daniel", "Juliana", "gato bob"];
+
+    console.log(miFamilia);
+
+for (let i = 0; i < miFamilia.length; i++) {
+    if (miFamilia[i] === "Isabel") {
+        console.log(miFamilia[i]);
+    }
+}
+
